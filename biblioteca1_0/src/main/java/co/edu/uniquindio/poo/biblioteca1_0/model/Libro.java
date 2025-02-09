4a0de807d9c4bd89e729ff402cd4d641d14a1ff4
glCristian <cristianc.bonillap@uqvirtual.edu.co>
2025-02-09 11:58:44 -0500

    prueba 0

--- biblioteca1_0/src/main/java/co/edu/uniquindio/poo/biblioteca1_0/model/Libro.java
+++ biblioteca1_0/src/main/java/co/edu/uniquindio/poo/biblioteca1_0/model/Libro.java
@@ -1,4 +1,8 @@
 package co.edu.uniquindio.poo.biblioteca1_0.model;
 
-public class Libro {
+public class Libro extends MaterialBibliografico {
+
+    public Libro(String titulo, String autor, String añoPublicacion, String isbn) {
+        super(titulo, autor, añoPublicacion, isbn);
+    }
 }
